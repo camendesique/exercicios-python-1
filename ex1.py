0257e3986b496815c921843333b178014658c9b7
Caique Mendes <mendescaique994@gmail.com>
2026-09-10 21:01:21 -0300

    Novos exercicios

---
+++ ex1.py
@@ -0,0 +1,5 @@
+lado = float(input("Digite o valor do lado do quadrado: "))
+
+area = lado ** 2 
+
+print(f"A área do quadrado com lado {lado} é: {area}")
---
+++ ex2.py
@@ -0,0 +1,5 @@
+salario_inicial = float(input("Digite o salario do funcionario: "))
+
+salario_final = salario_inicial * 1.15 
+
+print(f"O salário com reajuste é: {salario_final}")
